48dd987e4434b4155dcddd5e8ceb5cd6f63ea29f
Lauri Uosukainen (TAU) <lauri.uosukainen@tuni.fi>
2025-11-18 14:23:36 +0200

    Separated file handling to its own module

--- Avasoft_thorlabs_kikkare/main.py
+++ Avasoft_thorlabs_kikkare/main.py
@@ -22,7 +22,8 @@ from avaspec import *
 import thorlab_device_control as tlab
 from pylinkam import interface, sdk
 import globals
-import settings as windows
+import setting_windows as windows
+from file_handler import *
 
 
 main_ui_class, main_baseclass = pg.Qt.loadUiType("main_window.ui")
@@ -184,6 +185,9 @@ class MainWindow(main_ui_class, main_baseclass):
 
         globals.stopscanning = False
 
+        ttl_on(globals.dev_handle)
+        QtTest.QTest.qWait(100)
+
         if self.measurement_mode == "Scope":
 
             self.initPlot(globals.min_wavelength, globals.max_wavelength, None, "Wavelength (nm)", "Counts (#)")
@@ -217,6 +221,10 @@ class MainWindow(main_ui_class, main_baseclass):
                 time.sleep(0.01)
 
         ret = AVS_StopMeasure(globals.dev_handle)
+
+        ttl_off(globals.dev_handle)
+        QtTest.QTest.qWait(100)
+
         self.StartMeasBtn.setEnabled(True)
         self.PauseMeasBtn.setEnabled(False)
         self.StopMeasBtn.setEnabled(False)
@@ -520,80 +528,6 @@ def setRH(RH,plateau_tolerance):
                 if temp_rh_setpoint != humidity_setpoint:
                     print(f"Current RH Setpoint = {humidity_setpoint}")
                     connection.set_value(interface.StageValueType.MANUAL_HUMIDITY_SETPOINT, humidity_setpoint)
-
-
-def saveToNewFile(file_name, folder, x, y):
-    """
-    Saves a list of values to a new file --> overwrites an existing file of the name. Mainly used for saving
-    reference and dark spectra.
-
-    :param file_name: str, creates a .txt file with this name and saves the spectrum data to it
-    :param x [float], input list of spectrum values to save
-    :param y [float], input list of time/wavelength values corresponding to spectrum values
-    :return:
-    """
-
-    """
-    if os.path.exists(f"{file_name}.txt"):
-        i = 1
-        while os.path.exists(f"{file_name}%s.txt" % i):
-            i += 1
-        fh = f"{file_name}%s" % i
-    else:
-        fh = file_name
-    """
-
-    complete_file_path = os.path.join(PROJECT_PATH, folder)
-
-    if not os.path.exists(f"{complete_file_path}"):
-        os.makedirs(f"{complete_file_path}")
-
-    try:
-        with open(f"{complete_file_path}\\{file_name}.txt", "w") as fh:
-            for n in range(len(y)):
-                fh.write(f"{x[n]:.1f}\t{y[n]}\n")
-    except IndexError:
-        return
-
-
-def readShutterTimers():
-    file_output = []
-
-    complete_file_path = os.path.join(PROJECT_PATH, "shutter_timer.txt")
-    if not os.path.exists(complete_file_path):
-        return []
-
-    try:
-        with open(f"{complete_file_path}", "r") as fh:
-            for line in fh:
-                file_output.append(int(line.strip()))
-
-        return file_output
-
-    except ValueError:
-        return []
-
-
-def readSpectrumFile(parent, file_name):
-    """
-    Reads a spectrum/wavelength file and returns them as to separate lists
-
-    :param parent: PyQt widget for returning possible errors
-    :param file_name: str, file to be read
-    :return:
-    """
-    try:
-        with open(file_name, "r") as fh:
-            wavelength = []
-            spectrum = []
-            for line in fh:
-                div_line = line.split("\t")
-                wavelength.append(float(div_line[0]))
-                spectrum.append(float(div_line[1].strip()))
-
-        return wavelength, spectrum
-    except FileNotFoundError:
-        QMessageBox.information(parent, "Error", "Unable to read file with name '" + file_name + "'")
 
 
 def main():
